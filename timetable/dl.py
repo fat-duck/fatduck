0d7a71b11d893e53ebce09d1ed82af515b4cbf71
kklee998 <kahkinlee998@gmail.com>
2018-10-08 20:55:25 +0800

    added scheduler.py

--- timetable/dl.py
+++ timetable/dl.py
@@ -2,18 +2,20 @@ import requests
 import zipfile
 import os
 
-url = "http://lms.apiit.edu.my/intake-timetable/download_timetable/timetableCSV.zip"
-print("Downloading . . . . . . . .")
 
-r = requests.get(url)
+def main():
+    url = "http://lms.apiit.edu.my/intake-timetable/download_timetable/timetableCSV.zip"
+    print("Downloading . . . . . . . .")
 
-with open("timetable/timetableCSV.zip", "wb") as f:
-    print("Writing timetableCSV.zip. . . . . .")
-    f.write(r.content)
+    r = requests.get(url)
 
-with zipfile.ZipFile("timetable/timetableCSV.zip", "r") as unzip:
-    print("Unzipping timetableCSV.zip. . . . . . .")
-    unzip.extractall("timetable/")
-    print("Unzipping completed")
-    os.remove("timetable/timetableCSV.zip")
-    print("Deleting zip")
+    with open("timetable/timetableCSV.zip", "wb") as f:
+        print("Writing timetableCSV.zip. . . . . .")
+        f.write(r.content)
+
+    with zipfile.ZipFile("timetable/timetableCSV.zip", "r") as unzip:
+        print("Unzipping timetableCSV.zip. . . . . . .")
+        unzip.extractall("timetable/")
+        print("Unzipping completed")
+        os.remove("timetable/timetableCSV.zip")
+        print("Deleting zip")
