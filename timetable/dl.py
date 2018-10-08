c0647757ca9286f33d2cafb5c9946479c216a399
kklee998 <kahkinlee998@gmail.com>
2018-10-08 20:41:21 +0800

    Added dl.py

---
+++ timetable/dl.py
@@ -0,0 +1,19 @@
+import requests
+import zipfile
+import os
+
+url = "http://lms.apiit.edu.my/intake-timetable/download_timetable/timetableCSV.zip"
+print("Downloading . . . . . . . .")
+
+r = requests.get(url)
+
+with open("timetable/timetableCSV.zip", "wb") as f:
+    print("Writing timetableCSV.zip. . . . . .")
+    f.write(r.content)
+
+with zipfile.ZipFile("timetable/timetableCSV.zip", "r") as unzip:
+    print("Unzipping timetableCSV.zip. . . . . . .")
+    unzip.extractall("timetable/")
+    print("Unzipping completed")
+    os.remove("timetable/timetableCSV.zip")
+    print("Deleting zip")
