a104085abefddc575e2ff847f8af402cb544a983
Kelvin <kelvinlim_98@live.com>
2018-10-09 00:49:31 +0800

    Added server configurations and webhook

---
+++ KarmaBot/server.py
@@ -0,0 +1,26 @@
+import requests
+from envparse import env
+from flask import Flask, request
+
+env.read_envfile()
+
+app = Flask(__name__)
+
+bot_token = env('BOT_TOKEN')
+
+def get_url(method):
+  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+
+def process_message(update):
+    data = {}
+    data["chat_id"] = update["message"]["from"]["id"]
+    data["text"] = "I can hear you!"
+    r = requests.post(get_url("sendMessage"), data=data)
+
+@app.route("/{}".format(bot_token), methods=["POST"])
+def process_update():
+    if request.method == "POST":
+        update = request.get_json()
+        if "message" in update:
+            process_message(update)
+        return "ok!", 200
